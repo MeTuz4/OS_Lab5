e62721f559fce57060f6fd6bf79d539758303091
user <user@localhost.localdomain>
2020-12-27 14:33:20 +0300

    part3

---
+++ newmem.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "" > report.log
+declare -a array
+counter=0
+while [ 0 ]
+do
+    array+=(0 1 2 3 4 5 6 7 8 9)
+    let counter=$counter+10
+    if [[ $counter%1000000 -eq 0 ]]
+    then
+	echo $counter >> report.log
+    fi
+    if [[ $counter -eq 30000000 ]]
+    then
+	break
+    fi
+done
